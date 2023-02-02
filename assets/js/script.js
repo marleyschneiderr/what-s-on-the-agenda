78933c02a3386bd58b3440b7457640478aa674cb
marleyschneiderr <marley33@uw.edu>
2023-02-01 22:19:04 -0800

    Adding time blocks on html

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,23 +15,18 @@ $(function () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
 
 
+// TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements. HINT: How can the id attribute of each time-block be used to do this?
 // getting all the data that was stored in local storage, returns the string that matches in html 
-$("#1-hour.description").val(localStorage.getItem("1-hour"));
-$("#2-hour .description").val(localStorage.getItem("2-hour"));
-$("#3-hour .description").val(localStorage.getItem("3-hour"));
-$("#4-hour .description").val(localStorage.getItem("4-hour"));
-$("#5-hour .description").val(localStorage.getItem("5-hour"));
-$("#6-hour .description").val(localStorage.getItem("6-hour"));
-$("#7-hour .description").val(localStorage.getItem("7-hour"));
-$("#8-hour .description").val(localStorage.getItem("8-hour"));
+// $("#1-hour.description").val(localStorage.getItem("1-hour"));
+// $("#2-hour .description").val(localStorage.getItem("2-hour"));
+// $("#3-hour .description").val(localStorage.getItem("3-hour"));
+// $("#4-hour .description").val(localStorage.getItem("4-hour"));
+// $("#5-hour .description").val(localStorage.getItem("5-hour"));
+// $("#6-hour .description").val(localStorage.getItem("6-hour"));
+// $("#7-hour .description").val(localStorage.getItem("7-hour"));
+// $("#8-hour .description").val(localStorage.getItem("8-hour"));
 $("#9-hour .description").val(localStorage.getItem("9-hour"));
 $("#10-hour .description").val(localStorage.getItem("10-hour"));
 $("#11-hour .description").val(localStorage.getItem("11-hour"));
@@ -48,4 +43,8 @@ $("#21-hour .description").val(localStorage.getItem("21-hour"));
 $("#22-hour .description").val(localStorage.getItem("22-hour"));
 $("#23-hour .description").val(localStorage.getItem("23-hour"));
 $("#24-hour .description").val(localStorage.getItem("24-hour"));
+
+// TODO: Add code to display the current date in the header of the page.
+
+});
 
