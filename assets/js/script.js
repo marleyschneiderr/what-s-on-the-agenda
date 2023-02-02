8e63f38dca5ddc020ecee5879620dc9467d0f39d
marleyschneiderr <marley33@uw.edu>
2023-02-01 22:04:51 -0800

    Adding JS local storage connection to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,31 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+
+// getting all the data that was stored in local storage, returns the string that matches in html 
+$("#1-hour.description").val(localStorage.getItem("1-hour"));
+$("#2-hour .description").val(localStorage.getItem("2-hour"));
+$("#3-hour .description").val(localStorage.getItem("3-hour"));
+$("#4-hour .description").val(localStorage.getItem("4-hour"));
+$("#5-hour .description").val(localStorage.getItem("5-hour"));
+$("#6-hour .description").val(localStorage.getItem("6-hour"));
+$("#7-hour .description").val(localStorage.getItem("7-hour"));
+$("#8-hour .description").val(localStorage.getItem("8-hour"));
+$("#9-hour .description").val(localStorage.getItem("9-hour"));
+$("#10-hour .description").val(localStorage.getItem("10-hour"));
+$("#11-hour .description").val(localStorage.getItem("11-hour"));
+$("#12-hour .description").val(localStorage.getItem("12-hour"));
+$("#13-hour .description").val(localStorage.getItem("13-hour"));
+$("#14-hour .description").val(localStorage.getItem("14-hour"));
+$("#15-hour .description").val(localStorage.getItem("15-hour"));
+$("#16-hour .description").val(localStorage.getItem("16-hour"));
+$("#17-hour .description").val(localStorage.getItem("17-hour"));
+$("#18-hour .description").val(localStorage.getItem("18-hour"));
+$("#19-hour .description").val(localStorage.getItem("19-hour"));
+$("#20-hour .description").val(localStorage.getItem("20-hour"));
+$("#21-hour .description").val(localStorage.getItem("21-hour"));
+$("#22-hour .description").val(localStorage.getItem("22-hour"));
+$("#23-hour .description").val(localStorage.getItem("23-hour"));
+$("#24-hour .description").val(localStorage.getItem("24-hour"));
+
