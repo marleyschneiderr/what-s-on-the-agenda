6bd09f61df7e5860fc91e0302b4e908f079ba7c5
marleyschneiderr <marley33@uw.edu>
2023-02-02 20:16:27 -0800

    Trying to get text to save on screen after refresh

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,7 +31,7 @@ for(var i = 0; i < 24; i++) {
     var savedValue = localStorage.getItem(key);
     
     if (savedValue) {
-      textCalendar.text(savedValue);
+      $(id).children(".description").text(savedValue);
     }
 
     var currentHour = dayjs().hour();
@@ -46,30 +46,30 @@ for(var i = 0; i < 24; i++) {
 
 
 // TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements
-$("#1-hour.description").val(localStorage.getItem("1-hour"));
-$("#2-hour.description").val(localStorage.getItem("2-hour"));
-$("#3-hour.description").val(localStorage.getItem("3-hour"));
-$("#4-hour.description").val(localStorage.getItem("4-hour"));
-$("#5-hour.description").val(localStorage.getItem("5-hour"));
-$("#6-hour.description").val(localStorage.getItem("6-hour"));
-$("#7-hour.description").val(localStorage.getItem("7-hour"));
-$("#8-hour.description").val(localStorage.getItem("8-hour"));
-$("#9-hour.description").val(localStorage.getItem("9-hour"));
-$("#10-hour.description").val(localStorage.getItem("10-hour"));
-$("#11-hour.description").val(localStorage.getItem("11-hour"));
-$("#12-hour.description").val(localStorage.getItem("12-hour"));
-$("#13-hour.description").val(localStorage.getItem("13-hour"));
-$("#14-hour.description").val(localStorage.getItem("14-hour"));
-$("#15-hour.description").val(localStorage.getItem("15-hour"));
-$("#16-hour.description").val(localStorage.getItem("16-hour"));
-$("#17-hour.description").val(localStorage.getItem("17-hour"));
-$("#18-hour.description").val(localStorage.getItem("18-hour"));
-$("#19-hour.description").val(localStorage.getItem("19-hour"));
-$("#20-hour.description").val(localStorage.getItem("20-hour"));
-$("#21-hour.description").val(localStorage.getItem("21-hour"));
-$("#22-hour.description").val(localStorage.getItem("22-hour"));
-$("#23-hour.description").val(localStorage.getItem("23-hour"));
-$("#24-hour.description").val(localStorage.getItem("24-hour"));
+$("#1-hour .description").val(localStorage.getItem("1-hour"));
+$("#2-hour .description").val(localStorage.getItem("2-hour"));
+$("#3-hour .description").val(localStorage.getItem("3-hour"));
+$("#4-hour .description").val(localStorage.getItem("4-hour"));
+$("#5-hour .description").val(localStorage.getItem("5-hour"));
+$("#6-hour .description").val(localStorage.getItem("6-hour"));
+$("#7-hour .description").val(localStorage.getItem("7-hour"));
+$("#8-hour .description").val(localStorage.getItem("8-hour"));
+$("#9-hour .description").val(localStorage.getItem("9-hour"));
+$("#10-hour .description").val(localStorage.getItem("10-hour"));
+$("#11-hour .description").val(localStorage.getItem("11-hour"));
+$("#12-hour .description").val(localStorage.getItem("12-hour"));
+$("#13-hour .description").val(localStorage.getItem("13-hour"));
+$("#14-hour .description").val(localStorage.getItem("14-hour"));
+$("#15-hour .description").val(localStorage.getItem("15-hour"));
+$("#16-hour .description").val(localStorage.getItem("16-hour"));
+$("#17-hour .description").val(localStorage.getItem("17-hour"));
+$("#18-hour .description").val(localStorage.getItem("18-hour"));
+$("#19-hour .description").val(localStorage.getItem("19-hour"));
+$("#20-hour .description").val(localStorage.getItem("20-hour"));
+$("#21-hour .description").val(localStorage.getItem("21-hour"));
+$("#22-hour .description").val(localStorage.getItem("22-hour"));
+$("#23-hour .description").val(localStorage.getItem("23-hour"));
+$("#24-hour .description").val(localStorage.getItem("24-hour"));
 
 
 // });
